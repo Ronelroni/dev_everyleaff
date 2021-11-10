518b1bd1b4b1730cab5bf2501761dfdfaf4ce317
Ronelroni <ronelroni10@gmail.com>
2021-11-10 13:13:50 +0100

    Correction Step1

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -1,11 +1,5 @@
 FactoryBot.define do
-    factory :task do
-      name {'title'}
-      content {'content'}
-    end
-    factory :second_task, class: Task do
-      name {'title2'}
-      content {'content2'}
-    end
+  factory :task do
+    
   end
-  
+end
