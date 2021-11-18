7cb88e93cdc7801bcbd6b70682b1bea96ed686f9
Ronelroni <ronelroni10@gmail.com>
2021-11-18 18:41:09 +0100

    Step5

--- app/models/task.rb
+++ app/models/task.rb
@@ -4,9 +4,15 @@ class Task < ApplicationRecord
     scope  :order_by_deadline,  ->  {order(deadline: :desc)}
     scope  :order_by_priority_button,  ->  {order(priority: :desc)}
     scope  :order_by_priority,  ->  (priority){where(priority: priority)}
+    scope :label_search, -> (search_label){
+        tasks = Labeling.where(label_id: search_label)
+        ids = tasks.map{ |task| task.task_id } 
+        where(id: ids)
+    }
     scope  :order_by_status,  ->  (status){where(status: status)}
     scope  :title_search, -> (search_key){where("name LIKE ?","%#{search_key}%")}
     scope :kaminari, -> (kaminari_pages){page(kaminari_pages).per(5)}
+  
     paginates_per 50
     enum status: {
         unstarted: 0,
@@ -18,5 +24,9 @@ class Task < ApplicationRecord
         Medium:1,
         High: 2
     }
-    belongs_to :user
+    belongs_to :user, optional: true
+    has_many :labelings, dependent: :destroy
+    has_many :labels, through: :labelings, source: :label
+    
+    
 end
--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -2,15 +2,19 @@ FactoryBot.define do
   factory :task do
     name {'title'}
     content {'content'}
-    deadline{DateTime.now +1.day}
+    expiry_date {DateTime.now +1.day}
     status {"unstarted"}
     priority {"Low"}
+    user_id {nil}
   end
   factory :second_task, class: Task do
     name {'title2'}
     content {'content2'}
-    deadline {DateTime.now +2.day}
+    expiry_date {DateTime.now +2.day}
     status {"progress"}
     priority {"High"}
+    user_id {nil}
+
   end
+ 
 end
